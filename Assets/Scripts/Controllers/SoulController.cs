6a9e89fd22b277b35b0696ca9ccd6bd9f5135f69
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2022-12-22 03:53:43 +0900

    soul-scripting

--- Assets/Scripts/Controllers/SoulController.cs
+++ Assets/Scripts/Controllers/SoulController.cs
@@ -1,25 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class SoulController : MonoBehaviour
 {
     private Animator anim;
-    
-    
+    // 마우스로 클릭해서 인식 후 대화
+    private 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        anim= GetComponent<Animator>();
+        anim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+
+    private void OnMouseEnter()
+    {
+        anim.SetBool("Damage", true);
+        anim.SetInteger("DamageType", 1);
+        talkPanel.SetActive(true);
+        nametag.SetActive(true);
+        
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        if (Input.GetKey(KeyCode.A))
+        // 마우스로 클릭해서 인식 후 대화
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
         {
-            anim.SetBool("Damage",true);
-            anim.SetInteger("DamageType",1);
+            Debug.Log(hit.transform.gameObject);
+            //1. 한번만 재생될 수 있게.
+            //2. 마우스 위로 가져다 놓기만 해도. 클릭 말고는????
+
+            if (hit.transform.gameObject.tag == "Soul") 
+            {
+
+            }
+
+            else if (hit.transform.gameObject.tag == "Book")
+            {
+
+
+            }
+
             
         }
     }
